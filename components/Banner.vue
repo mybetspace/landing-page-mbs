d80d67c1f78c4c790d9705f960473817c861fc45
NLihtnov <nicholas.dutra@compasso.com.br>
2023-11-27 18:16:51 -0300

    feat: banner component and images

--- components/Banner.vue
+++ components/Banner.vue
@@ -2,16 +2,23 @@
   <div class="block">
     <div class="container">
       <div id="slide">
-        <div v-for="(item, index) in items" :key="index" :class="['item', `item-${index + 1}`]" :style="{ backgroundImage: `url(${item.image})` }">
-          <div class="content">
-            <div class="name">{{ item.name }}</div>
-            <div class="des">{{ item.description }}</div>
-            <button :class="item.buttonClass" role="button">{{ item.buttonText }}</button>
-          </div>
+        <div
+          v-for="(item, index) in items"
+          :key="index"
+          :class="['item', `item-${index + 1}`]"
+          :style="{ backgroundImage: `url(${'./../assets/banner1.jpg'})` }"
+        >
+        <div class="item-content">
+              <div class="name">{{ item.name }}</div>
+              <div class="des">{{ item.description }}</div>
+              <button :class="item.buttonClass" role="button">
+                {{ item.buttonText }}
+              </button>
+            </div>
         </div>
-      </div>
       <div class="dots-container">
         <span v-for="(dot, index) in dots" :key="index" class="dot"></span>
+      </div>
       </div>
     </div>
   </div>
@@ -20,36 +27,36 @@
 <script setup>
 const items = [
   {
-    name: 'MY BET SPACE1',
-    description: 'Afinal, quanto vale seu tempo?',
-    buttonClass: 'button-87',
-    buttonText: 'Economize seu tempo',
-    image: 'https://i.ibb.co/XCPm4pW/9e6d3473-9056-4642-af86-72194d414eb0.jpg',
+    name: "MY BET SPACE1",
+    description: "Afinal, quanto vale seu tempo?",
+    buttonClass: "button-87",
+    buttonText: "Economize seu tempo",
+    image: "https://i.ibb.co/XCPm4pW/9e6d3473-9056-4642-af86-72194d414eb0.jpg",
   },
   {
-    name: 'MY BET SPACE2',
-    description: 'Afinal, quanto vale seu tempo?',
-    buttonClass: 'button-87',
-    buttonText: 'Economize seu tempo',
-    image: 'https://i.ibb.co/9ssqyCg/4520feb3-ac3b-48c1-92db-68b757a8cde6.jpg',
+    name: "MY BET SPACE2",
+    description: "Afinal, quanto vale seu tempo?",
+    buttonClass: "button-87",
+    buttonText: "Economize seu tempo",
+    image: "https://i.ibb.co/9ssqyCg/4520feb3-ac3b-48c1-92db-68b757a8cde6.jpg",
   },
   {
-    name: 'MY BET SPACE3',
-    description: 'Afinal, quanto vale seu tempo?',
-    buttonClass: 'button-87',
-    buttonText: 'Economize seu tempo',
-    image: 'https://i.ibb.co/XCPm4pW/9e6d3473-9056-4642-af86-72194d414eb0.jpg',
+    name: "MY BET SPACE3",
+    description: "Afinal, quanto vale seu tempo?",
+    buttonClass: "button-87",
+    buttonText: "Economize seu tempo",
+    image: "https://i.ibb.co/XCPm4pW/9e6d3473-9056-4642-af86-72194d414eb0.jpg",
   },
 ];
 
-const dots = Array(items.length).fill('');
+const dots = Array(items.length).fill("");
 const handleCardClick = () => {
-  let cards = document.querySelectorAll('.item');
+  let cards = document.querySelectorAll(".item");
 
   cards.forEach(function (card) {
-    card.addEventListener('click', function (event) {
+    card.addEventListener("click", function (event) {
       let clickedItem = event.currentTarget;
-      document.getElementById('slide').prepend(clickedItem);
+      document.getElementById("slide").prepend(clickedItem);
     });
   });
 };
@@ -59,123 +66,171 @@ onMounted(() => {
 </script>
 
 <style scoped>
-  body{
-    background-color: #eaeaea;
-    overflow: hidden;
+body {
+  background-color: #eaeaea;
+  overflow: hidden;
 }
-.container{
-    position: absolute;
-    left:50%;
-    top:50%;
-    transform: translate(-50%,-50%);
-    width:1000px;
-    height:600px;
-    padding:50px;
-    background-color: #f5f5f5;
-    box-shadow: 0 30px 50px #dbdbdb;
+.container {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100%;
+  background-color: #f5f5f5;
+  box-shadow: 0 30px 50px #dbdbdb;
 }
-#slide{
-    width:max-content;
-    margin-top:50px;
+#slide {
+  width: max-content;
+  margin-top: 50px;
 }
-.item{
-    width:200px;
-    height:250px;
-    background-position: 50% 50%;
-    display: inline-block;
-    transition: 0.5s;
-    background-size: cover;
-    position: absolute;
-    z-index: 1;
-    top:50%;
-    transform: translate(0,-50%);
-    border-radius: 20px;
-    box-shadow:  0 30px 50px #505050;
-    cursor: pointer;
+.item {
+     background-image: url("./../assets/banner1.jpg");
+  width: 200px;
+  height: 250px;
+  background-position: 50% 50%;
+  display: inline-block;
+  transition: 0.5s;
+  background-size: cover;
+  position: absolute;
+  z-index: 1;
+  top: 50%;
+  transform: translate(0, -50%);
+  border-radius: 20px;
+  box-shadow: 0 30px 50px #505050;
+  cursor: pointer;
 }
-.item:nth-of-type(1){
-    left: 0;
-    top: 0;
-    transform: translate(0, 0);
-    border-radius: 0;
-    width: 100%;
-    height: 100%;
-    box-shadow: none;
+.item:nth-of-type(1) {
+  left: 0;
+  top: 0;
+  transform: translate(0, 0);
+  border-radius: 0;
+  width: 100%;
+  height: 100%;
+  box-shadow: none;
 }
 .item:nth-of-type(2) {
   left: 60%;
 }
 
 .item:nth-of-type(3) {
-    left: 80%;
+  left: 80%;
 }
-.item .content{
-    position: absolute;
-    top:50%;
-    left:-300%;
-    width:300px;
-    text-align: left;
-    padding:0;
-    color:#eee;
-    transform: translate(0,-50%);
-    display: none;
-    font-family: system-ui;
+.item-content {
+  display: flex;
+    flex-direction: column;
+    color: white;
+    height: 100%;
+    width: 27%;
+    justify-content: center;
+    align-items: flex-start;
+    margin-left: 5rem;
 }
-.item:nth-child(2) .content{
-    display: block;
-    z-index: 11111;
+
+/* @media (min-width: 768px) {
+  .item .content {
+    left: -265% !important; 
+  }
+  .item:nth-of-type(2) {
+    left: 56% !important;
+  }
+
+  .item:nth-of-type(3) {
+    left: 78% !important;
+  }
 }
-.item .name{
-    font-size: 40px;
-    font-weight: bold;
+
+@media (min-width: 992px) {
+  .item .content {
+    left: -265% !important; 
+  }
+  .item:nth-of-type(2) {
+    left: 56% !important;
+  }
+
+  .item:nth-of-type(3) {
+    left: 78% !important;
+  }
+}
+
+@media (min-width: 1200px) {
+  .item .content {
+    left: -600% !important; 
+  }
+  .item:nth-of-type(2) {
+    left: 56% !important;
+  }
+
+  .item:nth-of-type(3) {
+    left: 78% !important;
+  }
+}
+ */
+
+.item:not(:nth-child(1)) .name {
+  display: none;
+}
+
+.item:not(:nth-child(1)) .des {
+  display: none;
+}
+
+.item:not(:nth-child(1)) button {
+  display: none;
+}
+
+.item .name {
+  font-size: 40px;
+  font-weight: bold;
+  opacity: 0;
+  animation: showcontent 1s ease-in-out 1 forwards;
+}
+.item .des {
+  margin: 20px 0;
+  opacity: 0;
+  animation: showcontent 1s ease-in-out 0.3s 1 forwards;
+}
+.item button {
+  padding: 10px 20px;
+  border: none;
+  opacity: 0;
+  animation: showcontent 1s ease-in-out 0.6s 1 forwards;
+}
+@keyframes showcontent {
+  from {
     opacity: 0;
-    animation:showcontent 1s ease-in-out 1 forwards
-}
-.item .des{
-    margin:20px 0;
-    opacity: 0;
-    animation:showcontent 1s ease-in-out 0.3s 1 forwards
-}
-.item button{
-    padding:10px 20px;
-    border:none;
-    opacity: 0;
-    animation:showcontent 1s ease-in-out 0.6s 1 forwards
-}
-@keyframes showcontent{
-    from{
-        opacity: 0;
-        transform: translate(0,100px);
-        filter:blur(33px);
-    }to{
-        opacity: 1;
-        transform: translate(0,0);
-        filter:blur(0);
-    }
+    transform: translate(0, 100px);
+    filter: blur(33px);
+  }
+  to {
+    opacity: 1;
+    transform: translate(0, 0);
+    filter: blur(0);
+  }
 }
 .dots-container {
-               display: flex;
-    position: absolute;
-    z-index: 1;
-    width: 100%;
-    justify-content: center;
-    margin-top: 38.5rem;
-    align-items: flex-end;
+  display: flex;
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  justify-content: center;
+  margin-top: 38.5rem;
+  align-items: flex-end;
 }
 
-        .dot {
-            width: 0.7rem;
-            height: 0.7rem;
-            background-color: white;
-            border-radius: 50%;
-            margin-right: 1.5rem; 
-        }
+.dot {
+  width: 0.7rem;
+  height: 0.7rem;
+  background-color: white;
+  border-radius: 50%;
+  margin-right: 1.5rem;
+}
 
-        .dot:last-child {
-            margin-right: 0; 
-        }
+.dot:last-child {
+  margin-right: 0;
+}
 .dot.active {
-    background-color: #555; /* ou a cor desejada */
+  background-color: #555; /* ou a cor desejada */
 }
 
 .button-87 {
@@ -191,7 +246,12 @@ onMounted(() => {
   border: 0px;
   font-weight: 700;
   box-shadow: 0px 0px 14px -7px #f09819;
-  background-image: linear-gradient(45deg, #FF512F 0%, #F09819  51%, #FF512F  100%);
+  background-image: linear-gradient(
+    45deg,
+    #ff512f 0%,
+    #f09819 51%,
+    #ff512f 100%
+  );
   cursor: pointer;
   user-select: none;
   -webkit-user-select: none;
@@ -207,5 +267,4 @@ onMounted(() => {
 .button-87:active {
   transform: scale(0.95);
 }
-
 </style>
