66d92f50b53520b4a437bdaf1a29e44907e0665b
Icegreeen <flavioaquila16@gmail.com>
2024-01-23 22:17:13 -0300

    updated code

--- plugins/gtag.client.js
+++ plugins/gtag.client.js
@@ -3,25 +3,22 @@
 export default defineNuxtPlugin(() => {
   const { gtagId, DEVELOPMENT_MODE } = useRuntimeConfig().public;
 
-  if (DEVELOPMENT_MODE !== 'true') {
-
-    function gtag() {
-      window.dataLayer.push(arguments);
-    }
-
-    window.dataLayer = window.dataLayer || [];
-    gtag('js', new Date());
-    gtag('config', gtagId);
-
-    useHead({
-      script: [
-        {
-          src: `https://www.googletagmanager.com/gtag/js?id=${gtagId}`,
-          async: true,
-        },
-      ],
-    });
-  } else {
-    console.log('Plugin do GTM não está sendo executado em ambiente de produção');
+  if (DEVELOPMENT_MODE == 'true') return;
+  
+  function gtag() {
+    window.dataLayer.push(arguments);
   }
+
+  window.dataLayer = window.dataLayer || [];
+  gtag('js', new Date());
+  gtag('config', gtagId);
+
+  useHead({
+    script: [
+      {
+        src: `https://www.googletagmanager.com/gtag/js?id=${gtagId}`,
+        async: true,
+      },
+    ],
+  });
 });
