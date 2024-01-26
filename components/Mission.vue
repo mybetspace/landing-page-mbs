fb747234be49527b78603b0e579231ff061e1204
Jhonatan Sena <jcostasenna@hotmail.com>
2024-01-26 16:31:32 -0300

    chore: adjust the mission

--- components/Mission.vue
+++ components/Mission.vue
@@ -1,40 +1,37 @@
-<template>
-      <section class="relative items-center w-full">
-                <h2 class="mt-16 text-3xl font-bold text-white text-center dark:text-white sm:text-3xl">
-                    Nossa missão 
-                </h2>
-                
-                <div class="relative items-center w-full px-5 py-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
-                  <div class="relative flex-col items-start m-auto align-middle">
-                    <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
-                      <div class="relative items-center gap-12 m-auto ">
-                        <div class="max-w-xl text-center lg:text-left">
-                            <p class="text-2xl font-medium tracking-tight text-white  sm:text-3xl">
-                              Eficiência na Validação de Métodos
-                            </p>
-                            <p class="max-w-xl mt-4 text-base tracking-tight text-grayWhite">
-                              My Bet Space simplifica e agiliza a validação de métodos, economizando 
-                              tempo e ajudando os traders a atingirem maior lucratividade.
-                              My Bet Space simplifica e agiliza a validação de métodos, economizando tempo 
-                              e ajudando os traders a atingirem maior lucratividade.
-                            </p>
-                        </div>
+  <template>
+        <section class="relative items-center w-full">
+                  <h2 class="mt-16 text-3xl font-bold text-white text-center dark:text-white sm:text-3xl">
+                      Nossa missão 
+                  </h2>
+                  
+                  <div class="relative items-center w-full px-5 py-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
+                    <div class="relative flex-col items-start m-auto align-middle">
+                      <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
+                        <div class="relative items-center gap-12 m-auto ">
+                          <div class="max-w-xl text-center lg:text-left">
+                              <p class="text-2xl font-medium tracking-tight text-white  sm:text-3xl">
+                                Eficiência na Validação de Métodos
+                              </p>
+                              <p class="max-w-xl mt-4 text-base tracking-tight text-grayWhite">
+                                A My Bet Space tem como missão ajudar a comunidade de trader Esportivo a economizar seu tempo planilhando, auxiliar no dia a dia do trade, para que de forma mais ágil a validar consiga seu método, para ser mais lucrativo. 
+                              </p>
+                          </div>
 
-                        <div class="max-w-xl mt-8 text-center lg:text-left">
-                            <p class="text-2xl font-medium tracking-tight text-white sm:text-3xl">
-                              Automatização da Planilha Online
-                            </p>
-                            <p class="max-w-xl mt-4 text-base tracking-tight text-grayWhite">
-                              A My Bet Space atua como uma planilha online automatizada, coletando e analisando dados essenciais para que os usuários foquem apenas em cadastrar e analisar seus métodos.
-                            </p>
+                          <div class="max-w-xl mt-8 text-center lg:text-left">
+                              <p class="text-2xl font-medium tracking-tight text-white sm:text-3xl">
+                                Automatização da Planilha Online
+                              </p>
+                              <p class="max-w-xl mt-4 text-base tracking-tight text-grayWhite">
+                                Resumindo de uma forma simples a nossa plataforma é uma espécie de planilha online em que o usuário só precisa preencher o seu método e o resto a gente faz pra você. Pegamos todos os jogos, nomes dos times, mercados, campeonatos , odds, stake e entre outras coisas mais. Geramos diversos indicadores para você tomar as melhores decisões e conseguir validar seus métodos Você só precisa cadastrar seus métodos, preenche-los e analisar seus dados depois.
+                              </p>
+                          </div>
                         </div>
-                      </div>
-                      
-                      <div class="order-first block w-full mt-12 lg:mt-0">
-                          <img class="object-cover object-center w-full max-w-80 h-70 mx-auto" alt="cellphone" src="~/assets/images/telefone.png">
+                        
+                        <div class="order-first block w-full mt-12 lg:mt-0">
+                            <img class="object-cover object-center w-full max-w-80 h-70 mx-auto" alt="cellphone" src="~/assets/images/telefone.png">
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-      </section>
-</template>
+        </section>
+  </template>
