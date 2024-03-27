e0b8a8e7c2a2b90bb54fac2e7d9a66fb29c9cbda
Jhonatan Sena <jcostasenna@hotmail.com>
2024-03-27 11:07:53 -0300

    chore: add tags to improvi seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,7 @@ export default defineNuxtConfig({
     head: {
       title:
         'My Bet Space - Plataforma para Gestão de Banca para Trade Esportivo',
+        htmlAttrs: { lang: 'pt-br' },
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -30,6 +31,32 @@ export default defineNuxtConfig({
           content: `My Bet Space - Plataforma para Gestão de Banca para Trade Esportivo. Aqui você encontra uma melhor gestão das suas apostas esportivas da betfair,
           aumento no lucro dos seus investimentos e praticidade na sua vida como trader.`,
         },
+
+        { hid: 'application-name', name: 'application-name', content: 'My Bet Space' },
+        { hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: 'My Bet Space' },
+        { hid: 'author', name: 'author', content: 'My Bet Space' },
+        { hid: 'copyright', name: 'copyright', content: 'My Bet Space' },
+        { hid: 'creator', name: 'creator', content: 'My Bet Space' },
+
+            // Google Search Console
+        { hid: 'google-site-verification', name: 'google-site-verification', content: 'google487efc4e8cf55a11.html' },
+
+       
+
+        // Open Graph / Facebook and WhatsApp
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:title', property: 'og:title', content: 'My Bet Space' },
+        { hid: 'og:description', property: 'og:description', content: `My Bet Space - Plataforma para Gestão de Banca para Trade Esportivo. Aqui você encontra uma melhor gestão das suas apostas esportivas da betfair,
+        aumento no lucro dos seus investimentos e praticidade na sua vida como trader.` },
+        { hid: 'og:url', property: 'og:url', content: 'https://www.mybetspace.com/' },
+        { hid: 'og:image', property: 'og:image', content: '/og-image.png' },
+        { hid: 'og:image:width', property: 'og:image:width', content: '600' },
+        { hid: 'og:image:height', property: 'og:image:height', content: '315' },
+        { hid: 'og:image:alt', property: 'og:image:alt', content: 'My Bet Space' },
+        { hid: 'og:image:type', property: 'og:image:type', content: 'image/png' },
+        { hid: 'og:image:secure_url', property: 'og:image:secure_url', content: 'https://firebasestorage.googleapis.com/v0/b/mybetspace.appspot.com/o/banners%2Flogo.png?alt=media&token=79d030aa-8480-4a34-9328-70d87aad8829' },
+        { hid: 'og:image:alt', property: 'og:image:alt', content: 'My Bet Space' },
+        { hid: 'fb:page_id', property: 'fb:page_id', content: '108684993812345' },
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
