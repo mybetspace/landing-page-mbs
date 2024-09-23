152220e1d26831336f64803c0651357f358f1942
gustavonikov <gustavonikov@gmail.com>
2024-09-22 21:35:38 -0300

    fix: add expires header to imgs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const ONE_YEAR_IN_SECONDS = 365 * 24 * 60 * 60;
+const ONE_YEAR_IN_MILLISECONDS = ONE_YEAR_IN_SECONDS * 1000;
+const imagesHeaders = {
+  'Expires': new Date(Date.now() + ONE_YEAR_IN_MILLISECONDS).toUTCString(),
+}
+
 export default defineNuxtConfig({
   modules: ['@nuxtjs/google-fonts'],
 
@@ -8,10 +14,25 @@ export default defineNuxtConfig({
       DEVELOPMENT_MODE: process.env.DEVELOPMENT_MODE,
     }
   },
-  
+
   plugins: [
     '~/plugins/gtag.client.js',
   ],
+
+  routeRules: {
+    '/**/*.jpg': {
+      headers: imagesHeaders
+    },
+    '/**/*.jpeg': {
+      headers: imagesHeaders
+    },
+    '/**/*.png': {
+      headers: imagesHeaders
+    },
+    '/**/*.svg': {
+      headers: imagesHeaders
+    },
+  },
 
   app: {
     head: {
