5acc6ee9fd08ac647d66b52a48999e8edd56cec7
Jhonatan Sena <jcostasenna@hotmail.com>
2024-10-04 20:46:28 -0300

    refactor: remove unused objetct

--- plugins/gtag.client.js
+++ plugins/gtag.client.js
@@ -1,7 +1,7 @@
 // Nuxt plugin to configure Google Tag Manager
 
 export default defineNuxtPlugin(() => {
-  const { gtagId, secondGtagId, DEVELOPMENT_MODE } = useRuntimeConfig().public
+  const { gtagId, googleAdsId, DEVELOPMENT_MODE } = useRuntimeConfig().public
 
   if (DEVELOPMENT_MODE === 'true') return
 
@@ -17,14 +17,8 @@ export default defineNuxtPlugin(() => {
         children: `window.dataLayer = window.dataLayer || [];function gtag() {window.dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${gtagId}', {transport_type: 'beacon'})`,
         type: "text/javascript"
       },
-      {
-        src: `https://www.googletagmanager.com/gtag/js?id=${secondGtagId}`,
-        async: true,
-        name: 'ga-script-2',
-        type: "text/javascript"
-      },
       { 
-        children: `window.dataLayer = window.dataLayer || [];gtag('config', '${secondGtagId}', {transport_type: 'beacon'})`,
+        children: `gtag('config', '${googleAdsId}', {transport_type: 'beacon'})`,
         type: "text/javascript"
       }
     ],
