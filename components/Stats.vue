312b83886db9ed8fbc7865684c4dd84b78a05b0f
gustavonikov <gustavonikov@gmail.com>
2024-10-06 23:57:34 -0300

    fix: stats layout

--- components/Stats.vue
+++ components/Stats.vue
@@ -1,24 +1,36 @@
 <template>
-    <div class="container mx-auto mb-8 mt-8 xl:mt-16">
+    <div class="container mx-auto mb-4 mt-4 sm:mt-20">
       <section class="bg-gradient-to-tr from-indigo-700 via-blue-800 to-cyan-900  ">
       <div class="mx-auto max-w-screen-xl px-4 py-6">
-        <dl class="grid grid-cols-1 gap-4 sm:grid-cols-3 sm:divide-x sm:divide-white">
-          <div class="flex flex-col px-4 py-4 text-center">
-            <dt class="order-last text-grayWhite font-medium text-gray-500">Apostas registradas</dt>
-  
-            <dd class="text-4xl font-extrabold text-white md:text-5xl">950.235.793+</dd>
+        <dl class="grid grid-cols-1 items-center gap-4 sm:grid-cols-3 sm:divide-x sm:divide-white">
+          <div class="flex flex-col px-4 text-center">
+            <dd class="text-2xl font-extrabold text-white lg:text-3xl">
+              950.235.793+
+            </dd>
+
+            <dt class="order-last text-grayWhite font-medium text-gray-500">
+              Apostas registradas
+            </dt>
           </div>
   
           <div class="flex flex-col px-4 py-4 text-center">
-            <dt class="order-last text-grayWhite font-medium text-gray-500">Usuários cadastrados</dt>
-  
-            <dd class="text-4xl font-extrabold text-white md:text-5xl">18.951+</dd>
+            <dd class="text-2xl font-extrabold text-white lg:text-3xl">
+              18.951+
+            </dd>
+
+            <dt class="order-last text-grayWhite font-medium text-gray-500">
+              Usuários cadastrados
+            </dt>
           </div>
   
           <div class="flex flex-col px-4 py-4 text-center">
-            <dt class="order-last text-grayWhite font-medium text-gray-500">Quantidade de métodos criados</dt>
-  
-            <dd class="text-4xl font-extrabold text-white md:text-5xl">52.357+</dd>
+            <dd class="text-2xl font-extrabold text-white lg:text-3xl">
+              52.357+
+            </dd>
+
+            <dt class="order-last text-grayWhite font-medium text-gray-500">
+              Quantidade de métodos criados
+            </dt>
           </div>
         </dl>
       </div>
