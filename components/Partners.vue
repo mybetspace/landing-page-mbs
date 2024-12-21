93b80df800b94a25a57736cc9e6ceaf3ecaa8e43
Jhonatan Sena <jcostasenna@hotmail.com>
2024-12-21 17:36:06 -0300

    chore: adjust white labels icon

--- components/Partners.vue
+++ components/Partners.vue
@@ -3,41 +3,32 @@
     <h2 class="my-8 text-3xl font-bold text-white text-center dark:text-white sm:text-3xl">
       White labels - Integradas
     </h2>
-        <div class="my-8 mx-auto mt-8 max-w-[1420px]">
-          <div ref="slider" class="keen-slider">
-          <div class="keen-slider__slide number-slide7">
-            <NuxtImg src="/images/bolsaapostas.svg" alt="Bolsa de apostas" class="ml-14 h-14" loading="lazy" />
-          </div>
-          <div class="keen-slider__slide number-slide5">
-            <NuxtImg src="/images/fullbet.svg" alt="Full bet" class="ml-14 h-14" loading="lazy" />
-          </div>
-          <div class="keen-slider__slide number-slide8">
-            <NuxtImg src="/images/betfair.jpg" alt="betfair" class="ml-14 h-14" loading="lazy" />
-          </div>
-          <div class="keen-slider__slide number-slide3">
-            <NuxtImg src="/images/layback.svg" alt="Lay back" class="ml-14 h-14" loading="lazy" />
-          </div>
-          <div class="keen-slider__slide number-slide1">
-            <NuxtImg src="/images/levebet.svg" alt="Levebet" class="ml-14 h-14" loading="lazy" />
-          </div>
-          <div class="keen-slider__slide number-slide2">
-            <NuxtImg src="/images/apostasbf.svg" alt="Apostas bf" class="ml-14 h-14" loading="lazy" />
-          </div>
-          <div class="keen-slider__slide number-slide6">
-            <NuxtImg src="/images/betbra.svg" alt="Bet bra" class="ml-14 h-14" loading="lazy" />
-          </div>
-          <div class="keen-slider__slide number-slide8">
-            <NuxtImg src="/images/betfair.jpg" alt="betfair" class="ml-14 h-14" loading="lazy" />
-          </div>
-          <div class="keen-slider__slide number-slide8">
-            <NuxtImg src="/images/verdinhabet.svg" alt="Verdinha bet" class="ml-14 h-14" loading="lazy" />
-          </div>
-          <div class="keen-slider__slide number-slide8">
-            <NuxtImg src="/images/apostaNow.svg" alt="Apostas now" class="ml-14 h-14" loading="lazy" />
-          </div>
-        </div>
-        </div>
-      </section>
+    <div class="my-8 mx-auto mt-8 max-w-[1420px]">
+    <div ref="slider" class="keen-slider">
+      <div class="keen-slider__slide number-slide1">
+        <NuxtImg src="/images/bolsaapostas.svg" alt="Bolsa de apostas" class="ml-14 h-14" loading="lazy" />
+      </div>
+      <div class="keen-slider__slide number-slide2">
+        <NuxtImg src="/images/fullbet.svg" alt="Full bet" class="ml-14 h-14" loading="lazy" />
+      </div>
+      <div class="keen-slider__slide number-slide3">
+        <NuxtImg src="/images/betfair.jpg" alt="betfair" class="ml-14 h-14" loading="lazy" />
+      </div>
+      <div class="keen-slider__slide number-slide4">
+        <NuxtImg src="/images/betbra.svg" alt="Bet bra" class="ml-14 h-14" loading="lazy" />
+      </div>
+      <div class="keen-slider__slide number-slide5">
+        <NuxtImg src="/images/verdinhabet.svg" alt="Verdinha bet" class="ml-14 h-14" loading="lazy" />
+      </div>
+      <div class="keen-slider__slide number-slide6">
+        <NuxtImg src="/images/oddjusta.png" alt="Odd Justa" class="ml-14 h-14" loading="lazy" />
+      </div>
+      <div class="keen-slider__slide number-slide7">
+        <NuxtImg src="/images/betfair.jpg" alt="betfair" class="ml-14 h-14" loading="lazy" />
+      </div>
+    </div>
+  </div>
+    </section>
 </template>
 <script>
 import "keen-slider/keen-slider.min.css";
