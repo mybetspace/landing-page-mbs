bee92d32283c87487c9fab3be98d769243a659df
vinicius-eidy <zviniciuseidy2@gmail.com>
2025-06-08 18:35:48 -0300

    update: chat widget styles

--- components/ChatWidget.vue
+++ components/ChatWidget.vue
@@ -3,11 +3,11 @@ import { MessageCircleQuestion } from "lucide-vue-next";
 </script>
 
 <template>
-    <div
+    <a
+        href="https://t.me/MyBetSpacebot"
+        target="_blank"
         class="fixed cursor-pointer flex items-center justify-center bottom-5 right-5 rounded-full w-16 h-16 bg-[#2AA9EA] transition-all hover:bg-[#279dd8]"
     >
-        <a href="https://t.me/MyBetSpacebot" target="_blank">
-            <MessageCircleQuestion :size="36" :stroke-width="1.8" />
-        </a>
-    </div>
+        <MessageCircleQuestion :size="36" :stroke-width="1.8" />
+    </a>
 </template>
