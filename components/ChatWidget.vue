c5dbfff917c86bb1b0a35cf3aa9172e72e9df04a
martins <juliermesp@gmail.com>
2025-10-08 18:38:41 -0300

    change from telegram to whatsapp

--- components/ChatWidget.vue
+++ components/ChatWidget.vue
@@ -15,7 +15,7 @@ import MessageIcon from "./MessageIcon.vue";
         <TooltipRoot>
             <TooltipTrigger as-child>
                 <a
-                    href="https://t.me/MyBetSpacebot"
+                    href="http://wa.me/554899591818"
                     target="_blank"
                     class="fixed cursor-pointer flex items-center justify-center bottom-5 right-5 rounded-full w-16 h-16 bg-[#2AA9EA] transition-all hover:bg-[#279dd8]"
                 >
@@ -25,7 +25,7 @@ import MessageIcon from "./MessageIcon.vue";
 
             <TooltipPortal>
                 <TooltipContent side="left" class="p-2 rounded-lg bg-background shadow">
-                    Suporte Telegram
+                    Suporte WhatsApp
                     <TooltipArrow />
                 </TooltipContent>
             </TooltipPortal>
